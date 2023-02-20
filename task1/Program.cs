41c40de1af12dcf634b7b50af4aed7d808b6aa9e
«Natalia <kolodina_melbyk@mail.ru>
2023-02-20 13:40:02 +0400

    add task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿// Задача. Собрать строку с числами от a до b, a <= b.
+
+Console.Clear();
+string NumberFor(int a, int b)
+{
+    string result = String.Empty;
+    for (int i = a; i <= b; i++)
+    {
+        result += $"{i} ";
+    }
+    return result;
+}
+
+string NumberRec(int a, int b)
+{
+    if (a <= b)
+        return $"{a} " + NumberRec(a + 1, b);
+    else
+        return String.Empty;
+}
+
+Console.WriteLine(NumberFor(1, 10)); // 1 2 3 4 5 6 7 8 9 10
+Console.WriteLine(NumberRec(1, 10)); // 1 2 3 4 5 6 7 8 9 10
