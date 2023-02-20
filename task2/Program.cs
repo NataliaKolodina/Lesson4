81dca28d7e549d6d1bc77d615c397fe40c22c579
«Natalia <kolodina_melbyk@mail.ru>
2023-02-20 13:48:54 +0400

    add task2 Sum Reccursia

---
+++ task2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача. Сумма чисел от 1 до n.
+
+Console.Clear();
+
+int SumFor(int n)
+{
+    int res = 0;
+    for (int i = 1; i <= n; i++)
+        res += i;
+    return res;
+}
+
+int SumRec(int n)
+{
+    if (n == 1)
+        return 1;
+    else
+        return n + SumRec(n - 1);
+}
+
+Console.WriteLine(SumFor(10));
+Console.WriteLine(SumRec(10));
