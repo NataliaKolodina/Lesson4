1a34e07b3648dd90f000170ba94e13c7dff1f023
«Natalia <kolodina_melbyk@mail.ru>
2023-02-20 14:07:12 +0400

    add task2 Recursia Power

---
+++ task4/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача. Вычислить а в степеги n.
+
+Console.Clear();
+
+int PowerFor(int a, int n)
+{
+    int res = 1;
+    for (int i = 1; i <= n; i++)
+        res *= a;
+    return res;
+}
+
+int PowerRec(int a, int n)
+{
+    // return n==0 ? 1: PowerRec(a, n-1) *a;
+    if (n == 0)
+        return 1;
+    else
+        return PowerRec(a, n - 1) * a;
+}
+
+int PowerRecMath(int a, int n)
+{
+    if (n == 0)
+        return 1;
+    else if (n % 2 == 0)
+        return PowerRecMath(a * a, n / 2);
+    else
+        return PowerRecMath(a, n - 1) * a;
+}
+
+Console.WriteLine(PowerFor(2, 10));
+Console.WriteLine(PowerRec(2, 10));
+Console.WriteLine(PowerRecMath(2, 10));
