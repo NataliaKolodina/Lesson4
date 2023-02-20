38bbea2742b16d5a5d2f39888d31f7ea5a81ac1c
«Natalia <kolodina_melbyk@mail.ru>
2023-02-20 18:01:49 +0400

    add task7 Recursia Piramids

---
+++ task7/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задачаю Пирамидки. Даны три шпиля. На первом n количество колец пирамиды.
+// Нужно перемести кольца в том же порядке на третий шпиль
+
+Console.Clear();
+
+void Towers(string with = "1", string on = "3", string some = "2", int count = 3)
+// with - первый шпиль, откуда перекладываем
+// on - третий шпиль, куда перекладываем
+// some - второй шпиль, промежуточный
+// count - количество блинов
+{
+    if (count > 1)
+        Towers(with, some, on, count - 1);
+    Console.WriteLine($"{with} >> {on}");
+    if (count > 1)
+        Towers(some, on, with, count - 1);
+}
+
+Towers();
