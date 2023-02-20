524e233c39c23c8b468ea89b0dc15a4963bfd4aa
«Natalia <kolodina_melbyk@mail.ru>
2023-02-20 14:22:52 +0400

    add task2 Recursia Find Words

---
+++ task5/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача. Перебор слов.
+// В некотором машинном алфавите имеются 4 буквы.
+// Покажите все слова, состоящие из Т букв, которые можно построить из этого алфавита.
+
+Console.Clear();
+
+int n = 1;
+void FindWords(string alphabet, char[] word, int length = 0)
+{
+    if (length == word.Length)
+    {
+        Console.WriteLine($"{n++} {new String(word)}");
+        return;
+    }
+    for (int i = 0; i < alphabet.Length; i++)
+    {
+        word[length] = alphabet[i];
+        FindWords(alphabet, word, length + 1);
+    }
+}
+
+FindWords("аисв", new char[2]);
