5303e6e94e26fc5fd7cdd2c3513d4e4c8bd23eb1
«Natalia <kolodina_melbyk@mail.ru>
2023-02-20 13:55:54 +0400

    add task2 Recursia Factorial

---
+++ task3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача. Факториал числа
+
+Console.Clear();
+int FactorialFor(int n)
+{
+    int res = 1;
+    for (int i = 1; i <= n; i++)
+        res *= i;
+    return res;
+}
+
+int FactorialRec(int n)
+{
+    if (n == 0)
+        return 1;
+    else
+        return n * FactorialRec(n - 1);
+}
+
+Console.WriteLine(FactorialFor(10));
+Console.WriteLine(FactorialRec(10));
