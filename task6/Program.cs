63a3fafba605bfc6d9e47b45ebfaca1bf343abc9
«Natalia <kolodina_melbyk@mail.ru>
2023-02-20 17:19:53 +0400

    add task6 Directory & Files Info

---
+++ task6/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача. Информация о заданной папки
+
+Console.Clear();
+string path =
+    "/Users/Наталия/Desktop/Учеба GeekBrains/Знакомство с языком программирования С#/Lesson4"; // адрес нашей папки
+DirectoryInfo di = new DirectoryInfo(path); // DirectoryInfo - позволяет получить иформацию о директории
+Console.WriteLine(di.CreationTime); // дата создания папки
+
+FileInfo[] fi = di.GetFiles(); // какие файлы находятся в этой папке // FleInfo - позволяет получить иформацию о файлах
+for (int i = 0; i < fi.Length; i++)
+{
+    Console.WriteLine(fi[i].Name);
+}
